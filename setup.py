6d61fa221b66d8b9da8dab3f542e2ca22403f5b5
Nitin Tyagi <nktyagi423@gmail.com>
2023-09-24 12:16:57 +0530

    Project version updated

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_requirements(file_path:str)->List[str]:
 
 setup(
     name='mlproject',
-    version='0.0.0',
+    version='1.0.0',
     author='Nitin Kumar',
     author_email='nktyagi423@gmail.com',
     packages=find_packages(),
